9e7e1c1429457c903e9d0b119ae4e671d0bad875
Chamindu Sauranga <chamindu1567@gmail.com>
2021-12-17 00:04:01 +0530

    Completed week 03 H1 but not simulating. Possible error in the testbench

--- Week 03/AAC2M3H1/AAC2M3H1.v
+++ Week 03/AAC2M3H1/AAC2M3H1.v
@@ -35,16 +35,35 @@
 //////////////////////////////////////////////////////////////////////////////
 // 
   	                                            		
+module FullAdd1 (a,b,cin,sum,cout);
+    input a,b,cin;
+    output sum, cout;
+
+    wire n0,n1,n2;
+
+    xor xor0(n0,a,b);
+    xor xor1(Sum,n0,Cin);
+    and and0(n1,Cin,n0);
+    and and1(n2,a,b);
+    or or0(Cout,n1,n2);
+endmodule
+
 
 module FullAdd4( A,B,Cin,Sum,Cout);                	
-    input [3:0] A, B;, 
+    input [3:0] A, B; 
     input Cin; 			
     output [3:0] Sum;
     output Cout;
 
+
+    wire Sum0,Sum1,Sum2,Sum3,Cout0,Cout1,Cout2;
+
                    	          	
 // student code here
-
+    FullAdd1 f0(.a(A[0]),.b(B[0]),.cin(Cin),.sum(Sum[0]),.cout(Cout0));
+    FullAdd1 f1(.a(A[1]),.b(B[1]),.cin(Cout0),.sum(Sum[1]),.cout(Cout1));
+    FullAdd1 f2(.a(A[2]),.b(B[2]),.cin(Cout1),.sum(Sum[2]),.cout(Cout2));
+    FullAdd1 f3(.a(A[3]),.b(B[3]),.cin(Cout2),.sum(Sum[3]),.cout(Cout));
 
 endmodule // Majority  
 
